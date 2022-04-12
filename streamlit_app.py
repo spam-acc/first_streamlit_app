4c05c974aea38dcebed40610e8af841236e95595
spam-acc <103471943+spam-acc@users.noreply.github.com>
2022-04-12 12:08:54 +1000

    Update streamlit_app.py
    missing s in line 17

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 
 #pick list 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
-fruits_to_show = my_fruit_list.loc[fruit_selected]
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 #display the table
 streamlit.dataframe(fruits_to_show)
