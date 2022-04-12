14818ff9710ceecbd3568643db409219a721e784
spam-acc <103471943+spam-acc@users.noreply.github.com>
2022-04-12 16:22:22 +1000

    Update streamlit_app.py
    typos

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,11 +22,11 @@ streamlit.dataframe(fruits_to_show)
 import requests
 streamlit.header('Fruityvice Fruit Advice')
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?, 'Kiwi')
+fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
 streamlit.write('The user entered', fruit_choice)
 
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 #streamlit.text(fruityvice_response.json())
 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
