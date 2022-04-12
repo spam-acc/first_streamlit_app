93e4037f18b37539e07701918a2db920540b0ff4
spam-acc <103471943+spam-acc@users.noreply.github.com>
2022-04-12 16:15:38 +1000

    Update streamlit_app.py
    typo for json

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,6 +25,6 @@ streamlit.header('Fruityvice Fruit Advice')
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
 
-fruityvice_normalized = pandas.join_normalize(fruityvice_response.json())
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
 streamlit.dataframe(fruitvice_normalized)
