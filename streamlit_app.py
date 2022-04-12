07e422475b2af5ea35b3f6c3c22aea8eb6541adf
spam-acc <103471943+spam-acc@users.noreply.github.com>
2022-04-12 16:14:15 +1000

    Update streamlit_app.py
    normalized json

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,3 +24,7 @@ streamlit.header('Fruityvice Fruit Advice')
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+fruityvice_normalized = pandas.join_normalize(fruityvice_response.json())
+
+streamlit.dataframe(fruitvice_normalized)
