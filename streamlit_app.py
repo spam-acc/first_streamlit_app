9146dd7ccbead09055b3b48a707d85252fd31b24
spam-acc <103471943+spam-acc@users.noreply.github.com>
2022-04-12 11:49:15 +1000

    Update streamlit_app.py
    added menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Bluberry Oatmeal')
+streamlit.text('Kale, Spiange & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
